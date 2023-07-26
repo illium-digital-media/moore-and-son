75504a56701447977696bfc5cf75db2373586157
clayharrison8 <clayharrison8@gmail.com>
2023-07-27 00:30:40 +0100

    refactoring footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,89 +1,80 @@
+
 import React from "react";
 import Link from "next/link";
 
-const Footer = () => {
+type LinkType = {
+  href: string;
+  text: string;
+};
+
+type FooterLinkSectionProps = {
+  title: string;
+  links: LinkType[];
+};
+
+const FooterLinkSection: React.FC<FooterLinkSectionProps> = ({ title, links }) => (
+  <div className="flex-grow flex items-center justify-center">
+    <div className="max-lg:pb-5">
+      <h6 className="font-bold pb-2">{title}</h6>
+      {links.map((link, index) => (
+        <p key={index} className="pb-2">
+          <Link href={link.href}>{link.text}</Link>
+        </p>
+      ))}
+    </div>
+  </div>
+);
+
+const Footer: React.FC = () => {
+  const linkSections = [
+    {
+      title: "Company",
+      links: [
+        { href: "/", text: "Home" },
+        { href: "/about", text: "About Us" },
+        { href: "/contact", text: "Contact Us" },
+      ],
+    },
+    {
+      title: "Services",
+      links: [
+        { href: "/residential", text: "Residential Decorating" },
+        { href: "/commercial", text: "Commercial Decorating" },
+        { href: "/exterior", text: "Exterior Decorating" },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        { href: "/", text: "Facebook" },
+        { href: "/", text: "Instagram" },
+        { href: "/", text: "Pinterest" },
+        { href: "/", text: "YouTube" },
+        { href: "/", text: "TikTok" },
+      ],
+    },
+    {
+      title: "Contact",
+      links: [
+        { href: "/", text: "Email: exampleemail@gmail.com" },
+        { href: "/", text: "Phone: 0123 456 789" },
+      ],
+    },
+    // Add more sections as needed...
+  ];
+
   return (
-    <footer className="text-white">
-      <div className="m-auto">
-        <div className="bg-primary py-10">
-          <div className="max-w-screen-lg m-auto flex max-lg:block max-lg:text-center items-start">
-            <div className="flex-grow flex items-center justify-center">
-              <div className="max-lg:pb-5">
-                <h6 className="font-bold pb-2">Company</h6>
-                <p className="pb-2">
-                  <Link href="/">Home </Link>
-                </p>
-                <p className="pb-2">
-                  <Link href="/about">About Us </Link>
-                </p>
-                <p className="pb-2">
-                  <Link href="/contact">Contact Us </Link>
-                </p>
-              </div>
-            </div>
-            <div className="flex-grow flex items-center justify-center">
-              <div className="max-lg:pb-5">
-                <h6 className="font-bold pb-2">Services</h6>
-                <p className="pb-2">
-                  <Link href="/residential">Residential Decorating </Link>
-                </p>
-                <p className="pb-2">
-                  <Link href="/commercial">Commercial Decorating </Link>
-                </p>
-                <p className="pb-2">
-                  <Link href="/exterior">Exterior Decorating </Link>
-                </p>
-              </div>
-            </div>
-            <div className="flex-grow flex items-center justify-center">
-              <div className="max-lg:pb-5">
-                <h6 className="font-bold pb-2">Social</h6>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    Facebook
-                  </a>
-                </p>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    Instagram
-                  </a>
-                </p>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    Pinterest
-                  </a>
-                </p>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    Twitter
-                  </a>
-                </p>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    YouTube
-                  </a>
-                </p>
-                <p className="pb-2">
-                  <a href="" target="_blank">
-                    TikTok
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className="flex-grow flex items-center justify-center">
-              <div>
-                <h6 className="font-bold pb-2">Contact</h6>
-                <p className="pb-2">Email: info@theperfectpainter.co.uk</p>
-                <p className="pb-2">Phone: 0203 105 5111</p>
-              </div>
-            </div>
+    <footer className="text-white bg-gray-800">
+      <div className="max-w-screen-lg m-auto ">
+          <div className="flex max-lg:block max-lg:text-center items-start py-10">
+            {linkSections.map((section, index) => (
+              <FooterLinkSection key={index} title={section.title} links={section.links} />
+            ))}
           </div>
-        </div>
         <div className=" bg-gray-800 text-center">
-          <div className=" max-w-screen-lg m-auto p-2">
-            <p className="text-sm ">
-              &copy; 2023 Your Website. All Rights Reserved.
-            </p>
+          <hr />
+          <div className="p-2">
+            <p className="text-sm">&copy; 2023 Your Website. All Rights Reserved.</p>
           </div>
         </div>
       </div>
