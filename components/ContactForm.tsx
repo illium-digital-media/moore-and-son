b4efe68e3d0a0347eb297e5a8995a76188d828bf
clayharrison8 <clayharrison8@gmail.com>
2023-08-04 17:51:17 +0100

    reverting form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,7 +9,9 @@ const ContactForm = () => {
     message: "",
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
@@ -18,92 +20,60 @@ const ContactForm = () => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-
+    // Handle form submission logic here
+    console.log(formData);
+    // Reset the form
+    setFormData({ name: "", email: "", message: "" });
+    setIsSubmitted(true); // Set the submission state to true
   };
 
   return (
-    <form name="contact" className="" data-netlify="true">
-    {/* Use the name attribute for the input fields and textarea */}
-    <Input
-      value={formData.name}
-      handleInputChange={handleChange}
-      type="text"
-      id="name"
-      name="name"
-      placeholder="Name"
-    />
-    <Input
-      value={formData.email}
-      handleInputChange={handleChange}
-      type="email"
-      id="email"
-      name="email"
-      placeholder="Email"
-    />
-
-    <div className="mb-4">
-      <textarea
-        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-primary h-[150px]"
-        id="message"
-        name="message"
-        placeholder="Message"
-        value={formData.message}
-        onChange={handleChange}
-        required
-      ></textarea>
-    </div>
-    <button
-      className="bg-primary text-white font-bold py-2 px-4 rounded mt-4"
-      type="submit"
+    <form
+      name="contact"
+      className=""
+      onSubmit={handleSubmit}
+      data-netlify="true"
     >
-      Submit
-    </button>
-  </form>
-    
-    
+      <Input
+        value={formData.name}
+        handleInputChange={handleChange}
+        type={"text"}
+        id={"name"}
+        name={"name"}
+        placeholder={"Name"}
+      />
+      <Input
+        value={formData.email}
+        handleInputChange={handleChange}
+        type={"email"}
+        id={"email"}
+        name={"email"}
+        placeholder={"Email"}
+      />
 
-    // <form name="contact" className="" onSubmit={handleSubmit} data-netlify="true">
-    //   <Input
-    //     value={formData.name}
-    //     handleInputChange={handleChange}
-    //     type={"text"}
-    //     id={"name"}
-    //     name={"name"}
-    //     placeholder={"Name"}
-    //   />
-    //   <Input
-    //     value={formData.email}
-    //     handleInputChange={handleChange}
-    //     type={"email"}
-    //     id={"email"}
-    //     name={"email"}
-    //     placeholder={"Email"}
-    //   />
-
-    //   <div className="mb-4">
-    //     <textarea
-    //       className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-primary h-[150px]"
-    //       id="message"
-    //       name="message"
-    //       placeholder="Message"
-    //       value={formData.message}
-    //       onChange={handleChange}
-    //       required
-    //     ></textarea>
-    //   </div>
-    //   <button
-    //     className="bg-primary text-white font-bold py-2 px-4 rounded mt-4"
-    //     type="submit"
-    //   >
-    //     Submit
-    //   </button>
-    //   {isSubmitted && (
-    //     <div className="mt-4 p-2 bg-green-500 text-white rounded">
-    //       We have received your message and will get back to you shortly!
-    //     </div>
-    //   )}
-    // </form>
+      <div className="mb-4">
+        <textarea
+          className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-primary h-[150px]"
+          id="message"
+          name="message"
+          placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <button
+        className="bg-primary text-white font-bold py-2 px-4 rounded mt-4"
+        type="submit"
+      >
+        Submit
+      </button>
+      {isSubmitted && (
+        <div className="mt-4 p-2 bg-green-500 text-white rounded">
+          Thank you for your submission!
+        </div>
+      )}
+    </form>
   );
 };
 
