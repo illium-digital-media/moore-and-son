ca8c679262a6721fe8a3095ccf7370ff7888c627
clayharrison8 <clayharrison8@gmail.com>
2023-08-06 12:51:43 +0100

    adding gallery data in another file

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,58 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Example1 from "../assets/example-1.png";
-import Example2 from "../assets/example-2.png";
-import Example3 from "../assets/example-3.png";
 import Image from "next/image";
-
-const imageData = [
-  {
-    id: 1,
-    url: Example1,
-    category: "Exterior",
-  },
-  {
-    id: 2,
-    url: Example2,
-    category: "Interior",
-  },
-  {
-    id: 3,
-    url: Example3,
-    category: "Other",
-  },
-];
-
-var settings = {
-  dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  draggable: true,
-  centerMode: true,
-  centerPadding: "0px",
-  arrows: false,
-  variableWidth: true,
-  innerHeight: "40px",
-  // Default is true, but you can explicitly set it here.
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
+import { galleryPhotos } from "@/data/gallery";
 
 const serviceCategories = ["All", "Exterior", "Interior", "Other"];
 
@@ -64,8 +12,8 @@ const Gallery: React.FC = () => {
   // Filter images based on the selected category
   const filteredImages =
     selectedCategory === "All"
-      ? imageData
-      : imageData.filter((image) => image.category === selectedCategory);
+      ? galleryPhotos
+      : galleryPhotos.filter((image) => image.category === selectedCategory);
 
   // Handle category button clicks
   const handleCategoryClick = (category: string) => {
@@ -119,7 +67,7 @@ const Gallery: React.FC = () => {
             key={image.id}
             src={image.url}
             alt={image.category}
-            className={`mx-2 mb-3 transform scale-0 transition-all duration-500 ease-in-out hover:scale-200 ${
+            className={`max-w-xs mx-2 mb-3 transform scale-0 transition-all duration-500 ease-in-out hover:scale-200 ${
               fadeOut ? "opacity-0" : "opacity-100"
             }`}
             style={{ borderRadius: "8px" }}
