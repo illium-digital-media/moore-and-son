96ed7c9b61132a65d62eb9e96297a4b78196d14c
clayharrison8 <clayharrison8@gmail.com>
2023-10-13 19:56:08 +0100

    changed testimonials content

--- components/sections/Testimonials.js
+++ components/sections/Testimonials.js
@@ -1,61 +1,72 @@
-import React from 'react';
-import Title from '../Title/Title';
-import Topper from '../Topper/Topper';
-import styles from './Testimonials.module.css';
+import React from "react";
+import Title from "../Title/Title";
+import Topper from "../Topper/Topper";
+import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
-    const testimonialsData = [
-        {
-            starsSrc: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
-            text: "Curabitur vitae nunc sed velit dignissim sodales ut. Maecenas ultricies mi eget mauris pharetra et. Et ligula ullamcorper malesuada proin pellentesque diam volutpat commodo",
-            name: "Vera Robinson",
-            job: "Psychologist"
-        },
-        {
-            starsSrc: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
-            text: "Curabitur vitae nunc sed velit dignissim sodales ut. Maecenas ultricies mi eget mauris pharetra et. Et ligula ullamcorper malesuada proin pellentesque diam volutpat commodo",
-            name: "Benjamin Norris",
-            job: "Business analyst"
-        },
-        {
-            starsSrc: "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
-            text: "Curabitur vitae nunc sed velit dignissim sodales ut. Maecenas ultricies mi eget mauris pharetra et. Et ligula ullamcorper malesuada proin pellentesque diam volutpat commodo",
-            name: "Benjamin Norris",
-            job: "Business analyst"
-        }
-    ];
+  const testimonialsData = [
+    {
+      starsSrc:
+        "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
+      text: "Thank you Wayne, great job on refreshing the outside of our house, it looks lovely! Such a lovely guy an couldn't recommend him enough!",
+      name: "Amanda Walker",
+      job: "Homeowner",
+    },
+    {
+      starsSrc:
+        "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
+      text: "Well what can I say about this young man that's just decorated my complete bungalow he's done a grand job, also he's very reasonable with his prices, I would highly recommend him, thank you Wayne",
+      name: "Sheila Williams",
+      job: "Homeowner",
+    },
+    {
+      starsSrc:
+        "https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/stars3.svg",
+      text: "Would like to say huge thank you to Moore and Son for decorating my living room today I love it, done to perfection. Highly recommend and a lovely guy too, thanks once again.",
+      name: "Heather Gerrard",
+      job: "Homeowner",
+    },
+  ];
 
-    return (
-        <section className={styles.section}>
-            <div className={styles.container}>
-                <div className={styles.content}>
-                  <Topper text="Our Reviews" />
-                  <Title title='What Our Clients Say' />
-                    <p className={styles.text}>
-                        Vitae ultricies leo integer malesuada nunc vel. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Viverra aliquet eget sit amet tellus cras adipiscing. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit
-                    </p>
-                </div>
-                <ul className={styles.cardGroup}>
-                    {testimonialsData.map((testimonial, index) => (
-                        <li className={styles.item} key={index}>
-                            {/* Blue Quote */}
-                            <img className={styles.stars} aria-hidden="true" src={testimonial.starsSrc} decoding="async" alt="quote icon" width="189" height="24" />
-                            <p className={styles.itemText}>
-                                {testimonial.text}
-                            </p>
-                            <div className={styles.flexGroup}>
- 
-                                <span className={styles.name}>
-                                    {testimonial.name}
-                                    <span className={styles.job}>{testimonial.job}</span>
-                                </span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Topper text="Our Reviews" />
+          <Title title="What Our Clients Say" />
+          <p className={styles.text}>
+            At Moore and Son, your satisfaction is our success. Dive into the
+            experiences of our valued clients and discover why our craftsmanship
+            stands out. Their stories underscore our commitment to excellence,
+            one brushstroke at a time.{" "}
+          </p>
+        </div>
+        <ul className={styles.cardGroup}>
+          {testimonialsData.map((testimonial, index) => (
+            <li className={styles.item} key={index}>
+              {/* Blue Quote */}
+              <img
+                className={styles.stars}
+                aria-hidden="true"
+                src={testimonial.starsSrc}
+                decoding="async"
+                alt="quote icon"
+                width="189"
+                height="24"
+              />
+              <p className={styles.itemText}>{testimonial.text}</p>
+              <div className={styles.flexGroup}>
+                <span className={styles.name}>
+                  {testimonial.name}
+                  <span className={styles.job}>{testimonial.job}</span>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Testimonials;
--- components/sections/WhyChooseUs.js
+++ components/sections/WhyChooseUs.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Topper from '../Topper/Topper';
-import styles from './WhyChooseUs.module.css';
+import React from "react";
+import Topper from "../Topper/Topper";
+import styles from "./WhyChooseUs.module.css";
 
 const WhyChooseUs = () => {
   const items = [
     {
-      iconSrc: 'https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/award.svg',
-      title: 'Skilled Artisans',
-      description: 'Our seasoned team has transformed countless spaces, ensuring every project is a unique masterpiece tailored to the client\'s vision.',
+      iconSrc: "https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/award.svg",
+      title: "Skilled Artisans",
+      description:
+        "Our seasoned team has transformed countless spaces, ensuring every project is a unique masterpiece tailored to the client's vision.",
     },
     {
-      iconSrc: 'https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/sheild-black.svg',
-      title: 'Value-Packed Solutions',
-      description: 'Our services offer both quality and affordability, making top-tier painting and decorating accessible to all.',
+      iconSrc:
+        "https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/sheild-black.svg",
+      title: "Value-Packed Solutions",
+      description:
+        "Our services offer both quality and affordability, making top-tier painting and decorating accessible to all.",
     },
     {
-      iconSrc: 'https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/call.svg',
-      title: '100% Satisfaction',
-      description: 'Our commitment to excellence ensures unmatched customer satisfaction, turning first-time clients into lifelong patrons.',
+      iconSrc: "https://csimg.nyc3.digitaloceanspaces.com/Why-Choose/call.svg",
+      title: "100% Satisfaction",
+      description:
+        "Our commitment to excellence ensures unmatched customer satisfaction, turning first-time clients into lifelong patrons.",
     },
   ];
 
@@ -26,17 +30,30 @@ const WhyChooseUs = () => {
       <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.flex}>
-            <Topper text='Why Choose Us' />
-            <h2 className={styles.title}>Expert Painting & Decorating Service You Can Trust</h2>
+            <Topper text="Why Choose Us" />
+            <h2 className={styles.title}>
+              Expert Painting & Decorating Service You Can Trust
+            </h2>
           </div>
           <p className={styles.text}>
-          At Moore & Son, we bring color and precision to your spaces, allowing you to focus on what you do best. We enhance the ambiance and aesthetic of businesses and homes alike.
+            At Moore & Son, we bring color and precision to your spaces,
+            allowing you to focus on what you do best. We enhance the ambiance
+            and aesthetic of businesses and homes alike.
           </p>
         </div>
         <ul className={styles.cardGroup}>
           {items.map((item, index) => (
             <li className={styles.item} key={index}>
-              <img className={styles.icon} aria-hidden="true" loading="lazy" decoding="async" src={item.iconSrc} alt="icon" width="48" height="48" />
+              <img
+                className={styles.icon}
+                aria-hidden="true"
+                loading="lazy"
+                decoding="async"
+                src={item.iconSrc}
+                alt="icon"
+                width="48"
+                height="48"
+              />
               <h3 className={styles.h3}>{item.title}</h3>
               <p className={styles.itemText}>{item.description}</p>
             </li>
