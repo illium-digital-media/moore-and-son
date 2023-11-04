49f8dfa2089d90fd079d12e238319a1193f73414
clay <clayborne.harrison@sykescottages.co.uk>
2023-11-04 19:19:03 +0000

    fixing build error

--- components/sections/AboutMe.js
+++ components/sections/AboutMe.js
@@ -26,10 +26,10 @@ const AboutMe = () => {
           <Title title="Coventry's Go-To Painter" />
 
           <p className={styles.text}>
-            Hello, I'm Wayne, the driving force behind Moore and Son. With over a decade in the industry, my hands-on experience ranges from intricate interior work to robust exterior finishes. My father handed down not just his brushes, but a legacy of diligence and quality that Moore and Son upholds with every stroke.
+            Hello, I&apos;m Wayne, the driving force behind Moore and Son. With over a decade in the industry, my hands-on experience ranges from intricate interior work to robust exterior finishes. My father handed down not just his brushes, but a legacy of diligence and quality that Moore and Son upholds with every stroke.
           </p>
           <p className={styles.text}>
-            For me, painting is personal. You won't encounter salespeople or outsourced estimators here. I'm your first point of contact, ensuring a direct line of communication from the initial call through to the final brushstroke. At Moore and Son, we cut out the unnecessary to focus on what truly matters – exceptional painting services tailored to your needs.
+            For me, painting is personal. You won&apos;t encounter salespeople or outsourced estimators here. I&apos;m your first point of contact, ensuring a direct line of communication from the initial call through to the final brushstroke. At Moore and Son, we cut out the unnecessary to focus on what truly matters – exceptional painting services tailored to your needs.
           </p>
           <p className={styles.text}>
             My dedication is evident in the precision of our work and the pristine condition we leave behind. My promise to you is simple: we will strive to not only meet but exceed your expectations with every project. Ready to transform your space? Reach out directly or explore our gallery to see the difference passion makes.
