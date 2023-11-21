8991d63338ba45050a710f3b508ee5b1cda9bfe8
clay <clayborne.harrison@sykescottages.co.uk>
2023-11-21 16:17:13 +0000

    changing next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "www.mooreandsondecorators.com",
+    ],
+  },
+  output: "standalone",
 }
 
 module.exports = nextConfig
