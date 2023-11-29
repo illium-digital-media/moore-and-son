cb4a4c5959a5be744a41f5b2e0b2d9ff69e2d8c9
clay <clayborne.harrison@sykescottages.co.uk>
2023-11-29 19:27:30 +0000

    added ga hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,10 @@ import GalleryPreview from "../components/sections/GalleryPreview";
 import Services from "../components/sections/Services";
 import Contact from "../components/sections/Contact";
 import Script from "next/script";
-import { useEffect, useState } from 'react';
-
+import useLoadGAOnUserInteraction from "@/hooks/useLoadGAOnUserInteraction";
 
 const HomePage = () => {
-  const [loadGA, setLoadGA] = useState(false);
-
-  useEffect(() => {
-    const handleUserInteraction = () => {
-      setLoadGA(true);
-      cleanup();
-    };
-
-    const timerId = setTimeout(handleUserInteraction, 4000);
-
-    document.addEventListener('touchstart', handleUserInteraction, { once: true });
-    document.addEventListener('scroll', handleUserInteraction, { once: true });
-    document.addEventListener('mousemove', handleUserInteraction, { once: true });
-
-    const cleanup = () => {
-      clearTimeout(timerId);
-      document.removeEventListener('touchstart', handleUserInteraction);
-      document.removeEventListener('scroll', handleUserInteraction);
-      document.removeEventListener('mousemove', handleUserInteraction);
-    };
-
-    return cleanup;
-  }, []);
+  const loadGA = useLoadGAOnUserInteraction();
   return (
     <>
       {loadGA && (
@@ -42,11 +19,9 @@ const HomePage = () => {
           <Script
             id="gtm-script"
             src="https://www.googletagmanager.com/gtag/js?id=G-CQRNH0XQM6"
-            strategy="afterInteractive"
           />
           <Script
             id="gtm-script"
-            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
